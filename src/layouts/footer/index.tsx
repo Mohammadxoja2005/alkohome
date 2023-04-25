93f73e8bb3c111af6fd119ef5949369d43975e0e
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2023-04-26 03:01:00 +0500

    added final pattern to the end of the page

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC, Fragment } from 'react'
 // styles 
 import styles from "./index.module.scss";
 // icons
@@ -6,36 +6,40 @@ import CALL from "../../assets/icons/bgcall.png";
 import SMS from "../../assets/icons/bgsms.png";
 import VK from "../../assets/icons/bgvk.png";
 import TELEGRAM from "../../assets/icons/bgtelegram.png";
+// layouts
+import SECONDRAWIMG from '../seconrawimg';
 
-
-const FOOTER = () => {
+const FOOTER: FC = () => {
     return (
-        <section className={styles.footer}>
-            <div className={styles.footer_container}>
+        <Fragment>
+            <section className={styles.footer}>
+                <div className={styles.footer_container}>
 
-                <div className={styles.footer_navigation}>
-                    <nav className={styles.footer_nav_links}>
-                        <a className={styles.footer_nav_link} href="#">Главная</a>
-                        <a className={styles.footer_nav_link} href="#">Каталог</a>
-                        <a className={styles.footer_nav_link} href="#">Каталог</a>
-                        <a className={styles.footer_nav_link} href="#">Контакты</a>
-                        <a className={styles.footer_nav_link} href="#">Купить</a>
-                    </nav>
-                    <p className={styles.footer_document}>ИНН 7710912420 ОГРН 1127746354394 ООО «Деловой мир Москва»</p>
-                </div>
+                    <div className={styles.footer_navigation}>
+                        <nav className={styles.footer_nav_links}>
+                            <a className={styles.footer_nav_link} href="#">Главная</a>
+                            <a className={styles.footer_nav_link} href="#">Каталог</a>
+                            <a className={styles.footer_nav_link} href="#">Каталог</a>
+                            <a className={styles.footer_nav_link} href="#">Контакты</a>
+                            <a className={styles.footer_nav_link} href="#">Купить</a>
+                        </nav>
+                        <p className={styles.footer_document}>ИНН 7710912420 ОГРН 1127746354394 ООО «Деловой мир Москва»</p>
+                    </div>
 
-                <div className={styles.footer_contact} >
-                    <p className={styles.footer_contact_title}>Свяжитесь с нами</p>
+                    <div className={styles.footer_contact} >
+                        <p className={styles.footer_contact_title}>Свяжитесь с нами</p>
 
-                    <div className={styles.footer_contact_socials}>
-                        <img src={CALL} alt="call" />
-                        <img src={SMS} alt="sms" />
-                        <img src={VK} alt="vk" />
-                        <img src={TELEGRAM} alt="telegram" />
+                        <div className={styles.footer_contact_socials}>
+                            <img src={CALL} alt="call" />
+                            <img src={SMS} alt="sms" />
+                            <img src={VK} alt="vk" />
+                            <img src={TELEGRAM} alt="telegram" />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <SECONDRAWIMG />
+        </Fragment>
     )
 }
 
--- src/layouts/seconrawimg/index.tsx
+++ src/layouts/seconrawimg/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+// styles 
+import styles from "./index.module.scss";
+// images
+import RAW5 from "../../assets/images/row3.png";
 
-function index() {
+const SECONDRAWIMG = () => {
   return (
-    <div>index</div>
+    <section className={styles.secondraw}>
+      <img className={styles.secondraw_img} src={RAW5} alt="pattern" title='nice pattern' />
+    </section>
   )
 }
 
-export default index
+export default SECONDRAWIMG;
