ef18465034f592c6d20702b73a55f349cbcbd502
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2023-05-12 18:46:10 +0500

    added longer pattern

--- src/layouts/firstrawimg/index.tsx
+++ src/layouts/firstrawimg/index.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react'
 // styles
 import styles from "./index.module.scss";
 // images
-import RAW4 from "../../assets/images/row4.png";
+import RAW4 from "../../assets/images/row4.svg";
 
 const FIRSTRAWIMG: FC = () => {
     return (
         <section className={styles.firstraw}>
             <img className={styles.firstraw_img} src={RAW4} alt="pattern" title='nice pattern' />
-            <img className={styles.firstraw_img} src={RAW4} alt="pattern" title='nice pattern' />
+            {/* <img className={styles.firstraw_img} src={RAW4} alt="pattern" title='nice pattern' /> */}
         </section>
     )
 }
